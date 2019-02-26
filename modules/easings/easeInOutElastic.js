80c829cab5f1daa757d3db21190e60680f738d86
Simon Buerger <simon@bluegrassdigital.com>
2019-02-26 11:55:30 +0200

    optimisations

--- modules/easings/easeInOutElastic.js
+++ modules/easings/easeInOutElastic.js
@@ -1,23 +1,18 @@
-import { p, s, as, PI } from './_math'
+import { p, s, PI } from './_math'
 
 export default function (t) {
   var e,
-    n = 0.1,
     o = 0.4
   return t === 0
     ? 0
     : t === 1
       ? 1
-      : (!n || n < 1
-          ? ((n = 1), (e = o / 4))
-          : (e = o * as(1 / n) / (2 * PI)),
+      : ((e = o / 4),
         (t *= 2) < 1
           ? -0.5 *
-            n *
             p(2, 10 * (t -= 1)) *
             s(2 * (t - e) * PI / o)
-          : n *
-              p(2, -10 * (t -= 1)) *
+          : p(2, -10 * (t -= 1)) *
               s(2 * (t - e) * PI / o) *
               0.5 +
             1)
